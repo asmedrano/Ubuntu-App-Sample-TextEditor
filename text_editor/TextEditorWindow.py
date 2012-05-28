c91893a669c4df4078f8ed4f5efa9b3f842f6636
Angel Medrano <asmedrano@gmail.com>
2012-05-27 20:53:13 -0400

    adds Full CRUD

--- text_editor/TextEditorWindow.py
+++ text_editor/TextEditorWindow.py
@@ -25,7 +25,8 @@ class TextEditorWindow(Window):
     __gtype_name__ = "TextEditorWindow"
 
 	working_file_path = "" # keep track of the file we are working
-    
+    working_file_name = "Untitled"
+
     def finish_initializing(self, builder): # pylint: disable=E1002
         """Set up the main window"""
         super(TextEditorWindow, self).finish_initializing(builder)
@@ -35,52 +36,121 @@ class TextEditorWindow(Window):
         # Code for other initialization actions should be added here.
 
 	def file_new_handler(self, widget, data=None):
+		""" Resets TextBuffer
+		"""
 		print "CREATE NEW FILE"
-	
+
+		buff = self._get_buffer()
+		if buff.get_modified() == True:
+			print "There are changes, should we save them?"
+		else:
+			# clear out the buffer
+			buff.delete(buff.get_start_iter(), buff.get_end_iter())
+
+		
 	def file_save_handler(self, widget, data=None):
 		print "SAVING FILE"
+		self.save_buffer_to_file(widget)
+	
+	def save_buffer_to_file(self, widget):
+		""" Save working_file to disk
+			If we havent assigned working_file_path, this is a new file and we need to do the save AS routine
+		"""
+		if self.working_file_path == "":
+			self.save_buffer_to_file_AS(widget)
+		else:
+			try:
+				f = open(self.working_file_path, "w")
+				buff = self._get_buffer()
+				f.writelines(self._get_text())
+				#update modified flag
+				buff.set_modified(False)
+				f.close()
+
+			except IOError as e:
+				self.save_buffer_to_file_AS(widget)
+
 
 	def file_save_as_handler(self, widget, data=None):
+		""" Called when user selects, "Save As" """
 		print "SAVE AS..."
-
-	def file_open_handler(self, widget, data=None):
-		print "OPEN FILE"
-		dialog = Gtk.FileChooserDialog ("Open File", widget.get_toplevel(), Gtk.FileChooserAction.OPEN);
+		self.save_buffer_to_file_AS(widget)
+	
+	
+	def save_buffer_to_file_AS(self, widget):
+		""" Brings up Save As Dialog """
+		
+		dialog = Gtk.FileChooserDialog ("Save File As", widget.get_toplevel(), Gtk.FileChooserAction.SAVE);
 		dialog.add_button(Gtk.STOCK_CANCEL, 0)
 		dialog.add_button(Gtk.STOCK_OK, 1)
 		dialog.set_default_response(1)
 
-		f_filter = Gtk.FileFilter()
-		f_filter.add_pattern("*.txt")
+		if dialog.run() == 1:
+			file_selected = dialog.get_filename()
+			self.working_file_path = file_selected
+			buff = self._get_buffer()
+			
+			with open(self.working_file_path,"w") as f:
+				f.writelines(self._get_text())
 
-		dialog.set_filter(f_filter)
-
-		if dialog.run() ==1:
-			file_selected =dialog.get_filename()
-			# open file, read contents and send to TextBuff
-			with open(file_selected) as f:
-				data = f.read()
-				self._set_text(data)
-			self.working_file = file_selected
-
+			#update modified flag
+			buff.set_modified(False)
 		dialog.destroy()
-	
 
+
+	def file_open_handler(self, widget, data=None):
+		""" Opens an existing file. 
+			Checks to see if the buffer has been modified before opening it. 
+		"""
+
+		buff = self._get_buffer()
+		if buff.get_modified() == True:
+			print "There are changes, should we save them?"
+		else:
+			# clear out the buffer
+
+			dialog = Gtk.FileChooserDialog ("Open File", widget.get_toplevel(), Gtk.FileChooserAction.OPEN);
+			dialog.add_button(Gtk.STOCK_CANCEL, 0)
+			dialog.add_button(Gtk.STOCK_OK, 1)
+			dialog.set_default_response(1)
+
+			f_filter = Gtk.FileFilter()
+			f_filter.add_pattern("*.txt")
+
+			dialog.set_filter(f_filter)
+
+			if dialog.run() ==1:
+				file_selected =dialog.get_filename()
+				# open file, read contents and send to TextBuff
+				with open(file_selected) as f:
+					data = f.read()
+					buff.delete(buff.get_start_iter(), buff.get_end_iter())
+					buff.set_text(data)
+					buff.set_modified(False)
+
+				self.working_file_path = file_selected
+			
+			dialog.destroy()
+	
 
 	#---- helper methods----#
+
+	def _get_buffer(self):
+		"""returns main TextBuffer"""
+		return self.builder.get_object("write_view").get_buffer()
+
 	def _get_text(self):
 		""" Returns TextBuffer text"""
-		buff = self.builder.get_object("write_view").get_buffer()
+		buff = self._get_buffer()
+		start_iter = buff.get_start_iter()
+		end_iter = buff.get_end_iter()
 		
-		return buff.get_text(buff.get_start_iter(), buff.get_end_iter())
-	
+		return buff.get_text(start_iter, end_iter, False)
 
 	def _set_text(self, text):
 		""" Set text in TextBuffer"""
-		buff = self.builder.get_object("write_view").get_buffer()
+		buff = self._get_buffer()
 		buff.set_text(text)
 
 		return True
-
-
 	
